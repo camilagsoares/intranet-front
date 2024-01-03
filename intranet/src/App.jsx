4b6cfaee1ad9765b2173669b545b8ae5bbf0317f
camilagsoares <camila.soares19@hotmail.com>
2024-01-03 10:14:21 -0300

    .

--- intranet/src/App.jsx
+++ intranet/src/App.jsx
@@ -3,6 +3,7 @@ import { GlobalStyle } from "./styles/global"
 import Table from './components/Table'
 import Index from './pages/Index'
 import { BsArrowLeftShort } from "react-icons/bs";
+import { AiFillEnvironment } from "react-icons/ai";
 import { useState } from 'react';
 
 function App() {
@@ -11,8 +12,18 @@ function App() {
 
   return (
     <div className='flex'>
-      <div className='bg-dark-blue h-screen p-5 pt-8 w-72 relative'>
-        <BsArrowLeftShort className='bg-white text-dark-blue text-3xl rounded-full absolute -right-3 top-9 border-dark-blue cursor-pointer' />
+      <div className={`bg-dark-blue h-screen p-5 pt-8 ${open ? "w-72" : "w-20"} w-72 duration-300 relative`}>
+        <BsArrowLeftShort className={`bg-white text-dark-blue text-3xl
+         rounded-full absolute -right-3 top-9
+          border-dark-blue cursor-pointer 
+          ${!open && "rotate-180"}`}  
+          onClick={() => setOpen(!open)} 
+          />
+
+      <div>
+        <AiFillEnvironment className='bg-amber-300 text-4xl rounded cursor-pointer block float-left' />
+      </div>
+
       </div>
       <div className='p-7'></div>
       <h1 className='text-2xl font-semibold'>Home page</h1>
