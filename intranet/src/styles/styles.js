bccbfee18f8aff85e6c1f8e03b7023dc3bf626e8
camilagsoares <camila.soares19@hotmail.com>
2024-01-04 13:03:39 -0300

    .

---
+++ intranet/src/styles/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ContainerInput = styled.input`
+background: #dedede;
+height: 25px;
+width: 200px;
+border: 1px solid black;
+border-radius: 3px;
+padding: 2px;
+outline: none;
+`
