a4ac45c54808fbe8e624344bc9f95b075bad9816
camilagsoares <camila.soares19@hotmail.com>
2024-01-04 12:41:11 -0300

    .

--- intranet/src/components/Layout.jsx
+++ intranet/src/components/Layout.jsx
@@ -1,10 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import React from "react";
+
 
 export default function Layout() {
-    return(
-        <>
-        <Header />
-        </>
-    )
+	return (
+		<div className="bg-neutral-100 h-screen w-screen overflow-hidden flex flex-row">
+			<Header />
+		
+				<div className="flex-1 p-4 min-h-0 overflow-auto">
+					<Outlet />
+				</div>
+
+		</div>
+	)
 }
