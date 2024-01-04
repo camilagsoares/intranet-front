f498cec276466deef96f4b48ab556d3ba77d53cb
camilagsoares <camila.soares19@hotmail.com>
2024-01-04 12:40:41 -0300

    .

--- intranet/src/App.jsx
+++ intranet/src/App.jsx
@@ -1,33 +1,29 @@
 
 import Header from "./components/Header";
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./components/Layout";
 import Departamentos from "./pages/Departamentos";
 import Secretarias from "./pages/Secretarias";
 import Telefones from "./pages/Telefones";
-// import routes from "./routes";
+import Index from "./pages/Index";
 import Login from "./pages/Login";
-const App = () => {
+import Layout from "./components/Layout";
 
-  const router = createBrowserRouter([{
-    element: <Layout />,
-    errorElement: <Error />,
-    // children: routes
-  }])
+const App = () => {
 
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Header />} />
-        <Route path="/departamentos" element={<Departamentos />} />
-        <Route path="/secretarias" element={<Secretarias />} />
-        <Route path="/telefones" element={<Telefones />} />
-        <Route path="/login" element={<Login />} /> 
 
-      </Routes>
-    </Router>
+    <Router>
+    <Routes>
+        <Route path="/" element={<Layout />}>
+            <Route path="/departamentos" element={<Departamentos />} />
+            <Route path="/secretarias" element={<Secretarias />} />
+            <Route path="/telefones" element={<Telefones />} />
+        <Route path="/login" element={<Login />} />
+        </Route>
+   
+    </Routes>
+</Router>
   );
 };
 
