4825d3a5ab32cce5e9bd6e564747ed715a4f8953
camilagsoares <camila.soares19@hotmail.com>
2024-01-05 09:59:11 -0300

    .

---
+++ intranet/src/pages/Secretarias/Secretarias.jsx
@@ -0,0 +1,12 @@
+import Table from "./components/Table"
+
+
+const Secretarias = () => {
+    return (
+        <div>
+                <Table />
+        </div>
+    )
+}
+
+export default Secretarias;
