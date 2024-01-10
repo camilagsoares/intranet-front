cd6be12ea8e2773ed3905ec9d91ba1f8eed4a6ec
camilagsoares <camila.soares19@hotmail.com>
2024-01-10 13:48:10 -0300

    .

---
+++ intranet/src/pages/Departamentos/Departamentos.jsx
@@ -0,0 +1,15 @@
+import { useState, React } from "react";
+import TableT from "../../components/Table";
+
+
+const Departamentos = () => {
+
+
+    return (
+        <div className="flex flex-col gap-4">
+            <TableT />
+        </div>
+    );
+}
+
+export default Departamentos;
