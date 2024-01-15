914860a2e3aa12dfe267ed1715803505742b1e4f
camilagsoares <camila.soares19@hotmail.com>
2024-01-15 11:51:48 -0300

    .

--- intranet/src/pages/Departamentos/components/TableDepartamentos.jsx
+++ intranet/src/pages/Departamentos/components/TableDepartamentos.jsx
@@ -20,16 +20,8 @@ import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import AddIcon from '@mui/icons-material/Add';
 import { MdOutlineClose } from "react-icons/md";
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import IconButton from '@mui/material/IconButton';
-import Close from '@mui/icons-material/CloseOutlined';
-import Save from '@mui/icons-material/SaveAltOutlined';
+import ModalCriarDepartamento from './modalCriarDepartamento/ModalCriarDepartamento';
+import { MdOutlineModeEditOutline } from "react-icons/md";
 
 
 const TableDepartamentos = (props) => {
@@ -40,18 +32,7 @@ const TableDepartamentos = (props) => {
     const [pageNumber, setPageNumber] = useState(0);
     const [searchText, setSearchText] = useState('');
 
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: 'background.paper',
-        borderRadius: '5px',
-        boxShadow: 24,
-        p: 4,
-    };
-
+ 
 
     const StyledTableCell = styled(TableCell)(({ theme }) => ({
         [`&.${tableCellClasses.head}`]: {
@@ -103,7 +84,6 @@ const TableDepartamentos = (props) => {
     };
 
     const { data, loading } = useApiRequestGet('/departamento/listar-departamentos')
-    console.log(data)
 
     const projectsPerPage = 6;
     const pagesVisited = pageNumber * projectsPerPage;
@@ -126,8 +106,6 @@ const TableDepartamentos = (props) => {
     return (
         <React.Fragment>
             <Box marginY={1} paddingY={2}>
-
-
                 <Box sx={{ p: 1 }} style={{ display: 'flex', justifyContent: 'space-between' }}>
                     <Container>
                         <SearchIcon />
@@ -141,89 +119,24 @@ const TableDepartamentos = (props) => {
 
                     <Grid container justifyContent="flex-end">
                         <Button onClick={handleOpen} variant='outlined' startIcon={<AddIcon />}>Criar departamento</Button>
-                        <Modal
-                            open={open}
-                            onClose={handleClose}
-                            aria-labelledby="modal-modal-title"
-                            aria-describedby="modal-modal-description"
-                        >
-                            <Box sx={style}>
-                                <Box component='form' noValidate>
-                                    <DialogContent dividers sx={{ paddingTop: 1 }}>
-                                        <Grid container columnSpacing={2} rowSpacing={2} marginTop={0.5}>
-
-
-                                            <Grid item xs={12} sm={12} md={12}>
-                                                <TextField
-                                                    // {...register('titulo')}
-                                                    fullWidth
-                                                    required
-                                                    label='Título'
-                                                    type='text'
-                                                // error={!!errors.titulo}
-                                                // helperText={errors.titulo?.message}
-                                                />
-                                            </Grid>
-
-                                            <Grid item xs={12} sm={12} md={12}>
-                                                <TextField
-                                                    // {...register('valor')}
-                                                    fullWidth
-                                                    required
-                                                    label='Valor estimado. Exemplo: 31.000,98'
-                                                    type='text'
-                                                // error={!!errors.valor}
-                                                // helperText={valorError ? 'Não coloque ponto ou vírgula no campo de valor,se precisar arredonde' : errors.valor?.message}
-                                                />
-                                            </Grid>
-
-                                        </Grid>
-                                    </DialogContent>
-                                    <DialogActions>
-                                        <Button
-                                            // disabled={loading}
-                                            startIcon={<Close width={24} />}
-                                            variant='outlined'
-                                            color='info'
-                                            // onClick={handleFecharModalForm}
-                                            sx={{ minWidth: 156, height: '100%' }}
-                                        >
-                                            Cancelar
-                                        </Button>
-                                        <Button
-                                            type='submit'
-                                            // disabled={loading || isButtonDisabled}
-                                            // startIcon={<Save width={24} />}
-                                            variant='outlined'
-                                            color='success'
-                                            sx={{ minWidth: 156, height: '100%' }}
-                                        >
-                                            {/* {!loading ? 'Adicionar' : <CircularProgress color='success' size={23} />} */}
-                                        </Button>
-                                    </DialogActions>
-                                </Box>
-                            </Box>
-                        </Modal>
+                        <ModalCriarDepartamento onClick={handleOpen} />
                     </Grid>
                 </Box>
-
 
                 <TableContainer component={Paper}>
                     <Table sx={{ minWidth: 700 }} aria-label='customized table' >
                         <TableHead className='borda-azul'>
                             <StyledTableRow>
                                 <StyledTableCell align='left' width={112}>
-
                                     Id
                                 </StyledTableCell>
 
+                                <StyledTableCell width={300}>Departamento</StyledTableCell>
 
-                                <StyledTableCell width={192}>Departamento</StyledTableCell>
-
-                                <StyledTableCell align='left' width={180}>
+                                <StyledTableCell align='left' width={20}>
                                     Editar
                                 </StyledTableCell>
-                                <StyledTableCell align='left' width={180}>
+                                <StyledTableCell align='left' width={20}>
                                     Deletar
                                 </StyledTableCell>
 
@@ -244,7 +157,10 @@ const TableDepartamentos = (props) => {
 
 
                                         <StyledTableCell align="left" >
-                                            -
+                                        <Button>
+                                                <MdOutlineModeEditOutline  size={18} color='#68739C' />
+
+                                            </Button>
                                         </StyledTableCell>
                                         <StyledTableCell align="left" >
                                             <Button>
